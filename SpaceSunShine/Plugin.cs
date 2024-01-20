95b3f2d7e9c39629b586583f925d411fe0a1a51b
Itai Bergman <bergman.itai@gmail.com>
2024-01-20 14:01:58 +0200

    Add project files.

---
+++ SpaceSunShine/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SpaceSunShine
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string SpaceShipScene = "SampleSceneRelay";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != SpaceShipScene)
+            {
+                return;
+            }
+            GameObject[] SceneObjects = scene.GetRootGameObjects();
+            GameObject Environment = SceneObjects.Where(name => name.name == nameof(Environment)).ToArray()[0];
+            GameObject Lighting = Environment.transform.Find(nameof(Lighting)).gameObject;
+            GameObject Sun = Lighting.transform.Find(nameof(Sun)).gameObject;
+            Light SunLight = Sun.GetComponent<Light>();
+            SunLight.enabled = true;
+        }
+    }
+}
---
+++ SpaceSunShine/Thunderstore/README.md
@@ -0,0 +1,2 @@
+# Ship Sun Shine
+This mod adds the light outside of the ship when in space for a nice view of the planet you are orbiting.
